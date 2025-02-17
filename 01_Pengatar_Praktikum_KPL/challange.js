9a99cd1f6c1d506ecf358c644138990c57103c72
saputraardhian <ardhiandwisaputra767@gmail.com>
2025-02-17 16:26:35 +0700

    first meet

---
+++ 01_Pengatar_Praktikum_KPL/challange.js
@@ -0,0 +1,23 @@
+import process from "process";
+import readline from "readline";
+
+const input = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+input.question("Siapa nama kamu: ", (nama) => {
+  input.question("Jurusan kamu: ", (jurusan) => {
+    input.question("Universitas kamu: ", (universitas) => {
+      input.question("Umur kamu: ", (umur) => {
+        console.info(`\n========================`);
+        console.info(`Nama        : ${nama}`);
+        console.info(`Jurusan     : ${jurusan}`);
+        console.info(`Universitas : ${universitas}`);
+        console.info(`Umur        : ${umur} tahun`);
+
+        input.close();
+      });
+    });
+  });
+});
